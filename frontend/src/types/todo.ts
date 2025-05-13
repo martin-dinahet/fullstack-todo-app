07b538f5323160dea01477b670a1b6283895c229
Martin <martin.dinahet@proton.me>
2025-05-13 23:43:15 +0200

    wip: removed files from old branch & added next config

--- frontend/src/types/todo.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@/types/user";
-
-export interface Todo {
-  id: string;
-  content: string;
-  author: User;
-}
--- frontend/src/types/user.ts
+++
@@ -1,8 +0,0 @@
-import { Todo } from "@/types/todo";
-
-export interface User {
-  id: string;
-  username: string;
-  email: string;
-  todos: Array<Todo>;
-}
